37b28ef0067d5b7217f0844cf52d60b15e918150
Deyvison Ramos <deyvisonr0@gmail.com>
2020-01-24 05:30:18 -0300

    feat: cria e implementa serviços de troco

---
+++ ChangeApi.Domain/Services/Change.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeApi.Domain.Services
+{
+    public class Change : ServiceBase
+    {
+        public Change() : base()
+        {
+            AvailableCurrencyNotes = new List<int> { 50, 100, 20, 10 }.OrderByDescending(x => x).ToList();
+            AvailableCurrencyCoins = new List<int> { 10, 50, 5, 1 }.OrderByDescending(x => x).ToList();
+            Notes = new Dictionary<int, int>();
+            Coins = new Dictionary<int, int>();
+        }
+
+        private List<int> AvailableCurrencyNotes { get; }
+        private List<int> AvailableCurrencyCoins { get; }
+        /// <summary>
+        /// Key = note |=> Value = quantity
+        /// </summary>
+        public Dictionary<int, int> Notes { get; }
+
+        /// <summary>
+        /// Key = note |=> Value = quantity
+        /// </summary>
+        public Dictionary<int, int> Coins { get; }
+
+        public string Calc(decimal amountPaid, decimal totalAmount)
+        {
+            var amountToChange = totalAmount - amountPaid;
+            
+            if (amountToChange >= 0)
+            {
+                var message = "Nothing to change. There is lack of money in this payment or it doesn't need a change.";
+                AddError("amounts", message);
+                return message;
+            }
+
+            Notes.Clear();
+            Coins.Clear();
+
+            amountToChange *= -1;
+            var intAmount = (int) amountToChange;
+            var decimalAmount = (int)((amountToChange - intAmount) * 100);
+            var notesAndCoinsToGiveBack = new List<string>();
+
+            foreach (var availableCurrencyNote in AvailableCurrencyNotes)
+            {
+                if (intAmount - availableCurrencyNote < 0)
+                    continue;
+
+                int control = intAmount / availableCurrencyNote;
+                if (control != 0)
+                {
+                    notesAndCoinsToGiveBack.Add($"{control} de R${availableCurrencyNote}");
+                    Notes.Add(availableCurrencyNote, control);
+                    intAmount %= availableCurrencyNote;
+                }
+            }
+
+            var remainingAmount = decimalAmount + intAmount * 100;
+            if(remainingAmount > 0)
+            {
+                foreach (var availableCurrencyCoin in AvailableCurrencyCoins)
+                {
+                    if (remainingAmount - availableCurrencyCoin < 0)
+                        continue;
+
+                    int control = remainingAmount / availableCurrencyCoin;
+                    if (control != 0)
+                    {
+                        notesAndCoinsToGiveBack.Add($"{control} de {availableCurrencyCoin} centavos");
+                        Coins.Add(availableCurrencyCoin, control);
+                        remainingAmount %= availableCurrencyCoin;
+                    }
+                }
+            }
+
+            return string.Join(";\n", notesAndCoinsToGiveBack);
+        }
+
+        public decimal TotalChanged()
+        {
+            return Notes.Sum(x => x.Value * x.Key) + (Coins.Sum(x => ((decimal)x.Value / 100) * x.Key));
+        }
+    }
+}
---
+++ ChangeApi.Domain/Services/ServiceBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChangeApi.Domain.Services
+{
+    public abstract class ServiceBase
+    {
+        protected ServiceBase()
+        {
+            Errors = new Dictionary<string, string>();
+        }
+        private Dictionary<string, string> Errors { get; }
+        public bool IsValid => !Errors.Any();
+        protected void AddError(string property, string message)
+        {
+            Errors.Add(property, message);
+        }
+
+        public List<KeyValuePair<string, string>> GetErrors => Errors.ToList();
+    }
+}
