9c40c56fe3c80f3266c1ed6899098d23373a27cf
Deyvison Ramos <deyvisonr0@gmail.com>
2020-01-24 05:32:08 -0300

    feat: cria o mapeamento da entidade de histórico

---
+++ ChangeApi.Data/Mappings/ChangeHistoryMapping.cs
@@ -0,0 +1,19 @@
+﻿using ChangeApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeApi.Data.Mappings
+{
+    public class ChangeHistoryMapping : IEntityTypeConfiguration<ChangeHistory>
+    {
+        public void Configure(EntityTypeBuilder<ChangeHistory> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.HasMany(x => x.Itens).WithOne();
+        }
+    }
+}
