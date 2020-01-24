72faf81b05b9b775e4545cd1d747ddd41e671134
Deyvison Ramos <deyvisonr0@gmail.com>
2020-01-24 05:31:02 -0300

    feat: adiciona entidades de histórico

---
+++ ChangeApi.Domain/Entities/ChangeHistory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace ChangeApi.Domain.Entities
+{
+    public class ChangeHistory
+    {
+        public ChangeHistory()
+        {
+            Itens = new Collection<ChangeHistoryItem>();
+        }
+
+        public ChangeHistory(decimal totalChanged)
+        {
+            TotalChanged = totalChanged;
+            Itens = new Collection<ChangeHistoryItem>();
+        }
+
+        public int Id { get; set; }
+        public decimal TotalChanged { get; set; }
+        public virtual ICollection<ChangeHistoryItem> Itens { get; set; }
+
+        public void AddItem(ChangeHistoryItem item)
+        {
+            if (item != null)
+                Itens.Add(item);
+        }
+
+        public void AddItems(IEnumerable<ChangeHistoryItem> items)
+        {
+            if (items != null & items.Any())
+            {
+                foreach (var item in items)
+                {
+                    Itens.Add(item);
+                }
+            }
+                
+        }
+    }
+}
---
+++ ChangeApi.Domain/Entities/ChangeHistoryItem.cs
@@ -0,0 +1,24 @@
+﻿using ChangeApi.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeApi.Domain.Entities
+{
+    public class ChangeHistoryItem
+    {
+        public ChangeHistoryItem() {
+        }
+        public ChangeHistoryItem(ECurrencyType type, int quantity, int value)
+        {
+            Type = type;
+            Quantity = quantity;
+            Value = value;
+        }
+
+        public long Id { get; set; }
+        public ECurrencyType Type { get; set; }
+        public int Quantity { get; set; }
+        public int Value { get; set; }
+    }
+}
