b88d5ebe3e62300152bb0b6c7d801fea572cfe69
Deyvison Ramos <deyvisonr0@gmail.com>
2020-01-24 05:33:33 -0300

    feat: cria dto de calculo

---
+++ ChangeApi/Dto/CalculateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ChangeApi.Dto
+{
+    [DataContract]
+    public class CalculateDto
+    {
+        [DataMember(Name = "amountPaid")]
+        public decimal AmountPaid { get; set; }
+
+        [DataMember(Name = "totalAmountToPay")]
+        public decimal TotalAmountToPay { get; set; }
+    }
+}
