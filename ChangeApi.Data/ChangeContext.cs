2c3176e3616e297646f7777fef6fa76c1d07b0ad
Deyvison Ramos <deyvisonr0@gmail.com>
2020-01-24 05:31:22 -0300

    feat: cria o dbcontext da aplicação com ef core

---
+++ ChangeApi.Data/ChangeContext.cs
@@ -0,0 +1,25 @@
+﻿using ChangeApi.Data.Mappings;
+using ChangeApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeApi.Data
+{
+    public class ChangeContext : DbContext
+    {
+        public ChangeContext(DbContextOptions<ChangeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ChangeHistory> ChangeHistories { get; set; }
+        public DbSet<ChangeHistoryItem> ChangeHistoryItens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ChangeHistoryMapping());
+            base.OnModelCreating(builder);
+        }
+    }
+}
